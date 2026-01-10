e431dadfbd5dacfbf566943a1e88e3d16609121a
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-01-10 13:16:22 +0200

    Psalm Model implementation

--- lib/models/psalm.dart
+++ lib/models/psalm.dart
@@ -1,6 +1,19 @@
 class Psalm {
-  int? num;
-  String? text;
+  int num;
+  String description;
+  String text;
 
-  Psalm({this.num, this.text});
+  Psalm({required this.num, required this.description, required this.text});
+
+  getPsalmNumber() {
+    return this.num;
+  }
+
+  getPsalmDescription() {
+    return this.description;
+  }
+
+  getPsalmText() {
+    return this.text;
+  }
 }
