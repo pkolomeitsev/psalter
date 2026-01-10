02c9f2418d150e178c00ac6fd9225151dcd4c704
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-01-10 13:14:56 +0200

    implemented draft psalm JSON loading functionality

--- lib/pages/home/psalm.dart
+++ lib/pages/home/psalm.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jungers_psalter/models/psalm.dart' as psalm_model;
+import 'package:jungers_psalter/storage/psalm_storage.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class Psalm extends StatefulWidget {
@@ -10,6 +12,12 @@ class Psalm extends StatefulWidget {
 }
 
 class _PsalmState extends State<Psalm> {
+  late psalm_model.Psalm psalm;
+
+  Future<psalm_model.Psalm> fetchData() async {
+    return await PsalmStorage.getPsalmById(widget.psalmId ?? '');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +29,22 @@ class _PsalmState extends State<Psalm> {
         centerTitle: true,
         backgroundColor: Colors.indigo,
       ),
-      body: Placeholder(),
+      body: FutureBuilder<psalm_model.Psalm>(
+        future: this.fetchData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            return Center(
+              child: Text('Data loaded: ${snapshot.data!.getPsalmDescription()}'),
+            );
+          } else {
+            return const Center(child: Text('No data found'));
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/storage/psalm_storage.dart
@@ -0,0 +1,24 @@
+import 'package:jungers_psalter/models/psalm.dart';
+import 'package:jungers_psalter/storage/locale_storage.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+
+class PsalmStorage {
+  static Future<Psalm> getPsalmById(String psalmId) async {
+    String psalmKey = 'psalm$psalmId';
+    String psalmDescriptionKey = 'psalmDesc$psalmId';
+
+    String languageCode = await LocaleStorage.getLocale();
+
+    String filePath = 'assets/psalms/$languageCode.json';
+
+    final String response = await rootBundle.loadString(filePath);
+    final data = await json.decode(response);
+
+    return Psalm(
+      num: int.parse(psalmId),
+      description: data[psalmDescriptionKey] ?? '',
+      text: data[psalmKey] ?? '',
+    );
+  }
+}
