3247a1c2e74f17964486f889396f4dd26746c99e
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-01-23 02:08:29 +0200

    update PsalmStorage

--- lib/storage/psalm_storage.dart
+++ lib/storage/psalm_storage.dart
@@ -1,24 +1,44 @@
+import 'package:jungers_psalter/helpers/json_helper.dart';
 import 'package:jungers_psalter/models/psalm.dart';
 import 'package:jungers_psalter/storage/locale_storage.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'dart:convert';
 
 class PsalmStorage {
+
+  static Future<dynamic> getJsonData() async {
+    String languageCode = await LocaleStorage.getLocale();
+    String filePath = 'assets/data/psalms/$languageCode.json';
+
+    return await JsonHelper.getJsonData(filePath);
+  }
+
   static Future<Psalm> getPsalmById(String psalmId) async {
     String psalmKey = 'psalm$psalmId';
     String psalmDescriptionKey = 'psalmDesc$psalmId';
 
-    String languageCode = await LocaleStorage.getLocale();
-
-    String filePath = 'assets/data/psalms/$languageCode.json';
-
-    final String response = await rootBundle.loadString(filePath);
-    final data = await json.decode(response);
+    final data = await PsalmStorage.getJsonData();
 
     return Psalm(
       num: int.parse(psalmId),
       description: data[psalmDescriptionKey] ?? '',
       text: data[psalmKey] ?? '',
     );
+  }
+
+  static Future<List<Psalm>> getPsalmsByIds(List<int> ids) async {
+    final List<Psalm> result = [];
+    final data = await PsalmStorage.getJsonData();
+
+    for(final id in ids){
+      String psalmKey = 'psalm$id';
+      String psalmDescriptionKey = 'psalmDesc$id';
+
+      result.add(Psalm(
+        num: id,
+        description: data[psalmDescriptionKey] ?? '',
+        text: data[psalmKey] ?? '',
+      ));
+    }
+
+    return result;
   }
 }
