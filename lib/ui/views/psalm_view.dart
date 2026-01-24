b371c2ae49208ea4cf0829bca19ddae3a176b157
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-01-24 21:42:25 +0200

    implemented PsalmView widget

---
+++ lib/ui/views/psalm_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:jungers_psalter/models/psalm.dart';
+
+class PsalmView extends StatelessWidget {
+  final Psalm psalm;
+  const PsalmView({super.key, required this.psalm});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      DefaultTextStyle.merge(
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Text(this.psalm.getTitle() ?? '', textAlign: TextAlign.center),
+      ),
+      SizedBox(height: 10),
+      DefaultTextStyle.merge(
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Text(this.psalm.getDescription(), textAlign: TextAlign.center),
+      ),
+      SizedBox(height: 10),
+      DefaultTextStyle.merge(
+        style: const TextStyle(fontSize: 16),
+        child: Text(this.psalm.getText()),
+      ),
+    ]);
+  }
+}
