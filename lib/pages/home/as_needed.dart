53100c5b23145a9b3d56e5eb18574bdd05db14e9
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-01-31 00:31:52 +0200

    implemented renderPsalms logic for As Needed section

--- lib/pages/home/as_needed.dart
+++ lib/pages/home/as_needed.dart
@@ -1,8 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:jungers_psalter/storage/psalm_storage.dart';
 import 'package:jungers_psalter/ui/views/text_page_view_wrapper.dart';
 
 class AsNeeded extends StatefulWidget {
+  final int psalmAmount = (PsalmStorage.psalmsAmount - 1);
   const AsNeeded({super.key});
 
   @override
@@ -19,8 +21,10 @@ class _AsNeededState extends State<AsNeeded> {
           Column(
             children: [
               DefaultTextStyle.merge(
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight
-                    .bold),
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ),
                 child: Text(
                   context.tr('psalterReadingAsNeeded'),
                   textAlign: TextAlign.center,
@@ -33,8 +37,49 @@ class _AsNeededState extends State<AsNeeded> {
                     context.tr('psalterReadingRuleByStArseniosTheCappadocian')),
               ),
               SizedBox(height: 10),
+              DefaultTextStyle.merge(
+                style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+                child: Text(
+                    context.tr('psalterReadingAsNeededHelp')),
+              ),
+              SizedBox(height: 10),
+              this.renderPsalms(context),
             ],
           )
         ]);
+  }
+
+  Widget renderPsalms(BuildContext context) {
+    List<Widget> psalms = [];
+    for (var num = 1; num <= widget.psalmAmount; num++) {
+      String description = context.tr('psalm${num}AsNeeded');
+
+      psalms.add(
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(child: Text('$num.')),
+            Expanded(flex: 9, child: Text(description))
+          ],
+        )
+      );
+
+      psalms.add(SizedBox(height: 10));
+
+      if (num == 148) {
+        psalms.add(
+            DefaultTextStyle.merge(
+              style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+              child: Text(
+                  context.tr('psalm149150AsNeededHelp')),
+            )
+        );
+        psalms.add(SizedBox(height: 10));
+      }
+    }
+
+    return Column(
+      children: psalms,
+    );
   }
 }
