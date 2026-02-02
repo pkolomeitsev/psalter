20ce0933cc84237bb63f2a58e947b8ca8a87b7d9
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-02-03 01:55:35 +0200

    implemented BookmarkStorage deleteBookmarks() method

--- lib/storage/bookmark_storage.dart
+++ lib/storage/bookmark_storage.dart
@@ -17,12 +17,18 @@ class BookmarkStorage {
     box.add(value);
   }
 
-  static deleteBookmark(EntityType type, List<int> value) {
+  static deleteBookmark(EntityType type, int value) {
     final box = Hive.box(name: type.name);
     for (var i = 0; i < box.length; i++) {
       if (box.getAt(i) == value) {
         box.deleteAt(i);
       }
+    }
+  }
+
+  static deleteBookmarks(EntityType type, List<int> values) {
+    for(var value in values) {
+      BookmarkStorage.deleteBookmark(type, value);
     }
   }
 }
