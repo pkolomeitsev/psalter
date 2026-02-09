ed207b44ef7a2fb2e2917c7a348c68e64f5a3f9a
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-02-09 13:55:59 +0200

    implemented ConfirmDialog delete action for ChipList component

--- lib/ui/components/chip_list.dart
+++ lib/ui/components/chip_list.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jungers_psalter/interfaces/entity_object_interface.dart';
@@ -5,6 +6,7 @@ import 'package:jungers_psalter/storage/bookmark_storage.dart';
 
 class ChipList extends StatefulWidget {
   final List<EntityObjectInterface> chipList;
+
   const ChipList({super.key, required this.chipList});
 
   @override
@@ -31,19 +33,59 @@ class _ChipListState extends State<ChipList> {
           onSelected: (bool selected) {
             context.go('/${object.getType()}/${object.getId()}');
           },
-          onDeleted: () {
-            setState(() {
-              BookmarkStorage.deleteBookmark(object.getType(), object.getId());
-              this.chipListState.remove(object);
-            });
+          onDeleted: () async {
+            final bool? didConfirm = await deleteConfirmDialog(
+              context,
+              object.getTitle(),
+            );
+
+            if (didConfirm == true) {
+              setState(() {
+                BookmarkStorage.deleteBookmark(
+                  object.getType(),
+                  object.getId(),
+                );
+                this.chipListState.remove(object);
+              });
+            }
           },
         ),
       );
     }
 
-    return Wrap(
-        spacing: 5.0,
-        children: chips,
+    return Wrap(spacing: 5.0, children: chips);
+  }
+
+  Future<bool?> deleteConfirmDialog(
+    BuildContext context,
+    String chipName,
+  ) async {
+    return showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(
+            context.tr(
+              'deleteBookmarkConfirm',
+              namedArgs: {'chipName': chipName},
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text(context.tr('cancel')),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: Text(context.tr('delete')),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
