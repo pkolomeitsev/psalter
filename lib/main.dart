65e986e9e955a378e1ccde7707e2035e8d0200d0
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-02-16 16:41:49 +0200

    removed isar_flutter_libs as staled, reworked HiveDB logic for BookmarkStorage

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:orth_psalter/models/enums/entity_type.dart';
 import 'package:orth_psalter/pages/home/kathisma.dart';
 import 'package:orth_psalter/pages/home/psalm.dart';
 import 'package:orth_psalter/pages/main_application.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationDocumentsDirectory();
-  Hive.defaultDirectory = dir.path;
+  Hive.init(dir.path);
+
+  await Hive.openBox(EntityType.kathisma.name);
+  await Hive.openBox(EntityType.psalm.name);
+  await Hive.openBox(EntityType.asNeeded.name);
 
   runApp(EasyLocalization(
     supportedLocales: [
--- lib/storage/bookmark_storage.dart
+++ lib/storage/bookmark_storage.dart
@@ -4,7 +4,7 @@ import 'package:orth_psalter/models/enums/entity_type.dart';
 class BookmarkStorage {
   static getBookmarks(EntityType type) {
     List<int> bookmarksIds = [];
-    final box = Hive.box(name: type.name);
+    final box = Hive.box(type.name);
     for (var i = 0; i < box.length; i++) {
       bookmarksIds.add(box.getAt(i));
     }
@@ -13,12 +13,12 @@ class BookmarkStorage {
   }
 
   static addBookmark(EntityType type, int value) {
-    final box = Hive.box(name: type.name);
+    final box = Hive.box(type.name);
     box.add(value);
   }
 
   static deleteBookmark(EntityType type, int value) {
-    final box = Hive.box(name: type.name);
+    final box = Hive.box(type.name);
     for (var i = 0; i < box.length; i++) {
       if (box.getAt(i) == value) {
         box.deleteAt(i);
