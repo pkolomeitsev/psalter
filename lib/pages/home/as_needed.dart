97d1b726bc238f2c1edcb5f40576862160035976
Pavel Kolomeitsev <p.kolomeitsev@gmail.com>
2026-02-26 00:56:12 +0200

    Bugfix optimized as needed page (#3)
    * SizedBox clean-up
    
    * performance improvement for As Needed page
    
    * moved EntityObjectInterface to models directory

--- lib/pages/home/as_needed.dart
+++ lib/pages/home/as_needed.dart
@@ -4,10 +4,10 @@ import 'package:orth_psalter/models/enums/entity_type.dart';
 import 'package:orth_psalter/storage/bookmark_storage.dart';
 import 'package:orth_psalter/storage/psalm_storage.dart';
 import 'package:orth_psalter/ui/components/bookmark_card.dart';
-import 'package:orth_psalter/ui/views/text_page_view_wrapper.dart';
 
 class AsNeeded extends StatefulWidget {
   final int psalmAmount = (PsalmStorage.psalmsAmount - 1);
+
   const AsNeeded({super.key});
 
   @override
@@ -19,65 +19,57 @@ class _AsNeededState extends State<AsNeeded> {
 
   @override
   Widget build(BuildContext context) {
-    return TextPageViewWrapper(
-        data: [
-          Column(
-            children: [
-              DefaultTextStyle.merge(
-                style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                ),
-                child: Text(
-                  context.tr('psalterReadingAsNeeded'),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(height: 10),
-              DefaultTextStyle.merge(
-                style: const TextStyle(fontSize: 16),
-                child: Text(
-                    context.tr('psalterReadingRuleByStArseniosTheCappadocian')),
-              ),
-              SizedBox(height: 10),
-              this.renderPsalms(context),
-            ],
-          )
+    return ListView(padding: const EdgeInsets.all(10),
+        children: [
+          DefaultTextStyle.merge(
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+            child: Text(
+              context.tr('psalterReadingAsNeeded'),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 10),
+          DefaultTextStyle.merge(
+            style: const TextStyle(fontSize: 16),
+            child: Text(
+              context.tr('psalterReadingRuleByStArseniosTheCappadocian'),
+            ),
+          ),
+          SizedBox(height: 10),
+          ...this.renderPsalms(context)
         ]);
   }
 
-  Widget renderPsalms(BuildContext context) {
+  List<Widget> renderPsalms(BuildContext context) {
     List<Widget> psalms = [];
     List<int> bookmarks = BookmarkStorage.getBookmarks(EntityType.asNeeded);
 
     for (var i = 1; i <= widget.psalmAmount; i++) {
-      String description = context.tr('psalm${i}AsNeeded');
-
       psalms.add(
-          BookmarkCard(
-            id: i,
-            title: '${context.tr('psalm')} $i',
-            description: description,
-            type: EntityType.asNeeded,
-            isBookmarked: bookmarks.contains(i),
-          )
+        BookmarkCard(
+          id: i,
+          title: '${context.tr('psalm')} $i',
+          description: context.tr('psalm${i}AsNeeded'),
+          type: EntityType.asNeeded,
+          isBookmarked: bookmarks.contains(i),
+        ),
       );
 
-      psalms.add(SizedBox(height: 10));
-
       if (i == 148) {
+        psalms.add(SizedBox(height: 10));
         psalms.add(
-            DefaultTextStyle.merge(
-              style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-              child: Text(context.tr('psalm149150AsNeededHelp')),
-            )
+          DefaultTextStyle.merge(
+            style: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+            child: Text(context.tr('psalm149150AsNeededHelp')),
+          ),
         );
         psalms.add(SizedBox(height: 10));
       }
     }
 
-    return Column(
-      children: psalms,
-    );
+    return psalms;
   }
 }
