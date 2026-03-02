7330037c1d3f3b9f98eec510f9e37e08c26dedb7
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-03-02 12:34:20 +0200

    BookmarkStorage refactoring

--- lib/storage/bookmark_storage.dart
+++ lib/storage/bookmark_storage.dart
@@ -2,9 +2,14 @@ import 'package:hive/hive.dart';
 import 'package:orth_psalter/models/enums/entity_type.dart';
 
 class BookmarkStorage {
+
+  static getName(String name) {
+    return 'bookmarks_$name';
+  }
+
   static getBookmarks(EntityType type) {
     List<int> bookmarksIds = [];
-    final box = Hive.box(type.name);
+    final box = Hive.box(BookmarkStorage.getName(type.name));
     for (var i = 0; i < box.length; i++) {
       bookmarksIds.add(box.getAt(i));
     }
@@ -13,12 +18,12 @@ class BookmarkStorage {
   }
 
   static addBookmark(EntityType type, int value) {
-    final box = Hive.box(type.name);
+    final box = Hive.box(BookmarkStorage.getName(type.name));
     box.add(value);
   }
 
   static deleteBookmark(EntityType type, int value) {
-    final box = Hive.box(type.name);
+    final box = Hive.box(BookmarkStorage.getName(type.name));
     for (var i = 0; i < box.length; i++) {
       if (box.getAt(i) == value) {
         box.deleteAt(i);
