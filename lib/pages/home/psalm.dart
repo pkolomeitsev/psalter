67928f1179fc81bf3bf55195ef679228cac82f9b
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-03-03 03:12:19 +0200

    implemented LastViewed functionality for Psalms page

--- lib/pages/home/psalm.dart
+++ lib/pages/home/psalm.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:orth_psalter/models/enums/entity_type.dart';
 import 'package:orth_psalter/models/psalm.dart' as psalm_model;
+import 'package:orth_psalter/storage/last_viewed_storage.dart';
 import 'package:orth_psalter/storage/psalm_storage.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:orth_psalter/theme/app_font.dart';
 import 'package:orth_psalter/ui/views/text_page_view_wrapper.dart';
 
 class Psalm extends StatefulWidget {
-  final String? psalmId;
+  final String psalmId;
 
   const Psalm({super.key, this.psalmId = ''});
 
@@ -16,7 +18,13 @@ class Psalm extends StatefulWidget {
 
 class _PsalmState extends State<Psalm> {
   Future<psalm_model.Psalm> fetchData() async {
-    return await PsalmStorage.getPsalmById(widget.psalmId ?? '');
+    return await PsalmStorage.getPsalmById(widget.psalmId);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    LastViewedStorage.set(EntityType.psalm, int.parse(widget.psalmId));
   }
 
   @override
--- lib/pages/home/psalms.dart
+++ lib/pages/home/psalms.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:orth_psalter/models/enums/entity_type.dart';
+import 'package:orth_psalter/models/notifiers/last_viewed_psalms_notifier.dart';
 import 'package:orth_psalter/storage/bookmark_storage.dart';
+import 'package:orth_psalter/storage/last_viewed_storage.dart';
 import 'package:orth_psalter/storage/psalm_storage.dart';
 import 'package:orth_psalter/ui/components/bookmark_card.dart';
 import 'package:orth_psalter/ui/views/list_view_wrapper.dart';
@@ -15,24 +17,46 @@ class Psalms extends StatefulWidget {
 
 class _PsalmsState extends State<Psalms> {
   int psalmsAmount = PsalmStorage.psalmsAmount;
+  int lastViewedState = 0;
+  List<int> bookmarks = [];
+  final LastViewedPsalmsNotifier psalmsNotifier = LastViewedPsalmsNotifier();
+
+  @override
+  void initState() {
+    super.initState();
+    this.lastViewedState = LastViewedStorage.get(EntityType.psalm);
+    this.bookmarks = BookmarkStorage.getBookmarks(EntityType.psalm);
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> psalms = [];
-    List<int> bookmarks = BookmarkStorage.getBookmarks(EntityType.psalm);
+    return ListenableBuilder(
+      listenable: psalmsNotifier,
+      builder: (BuildContext context, Widget? child) {
+        if (psalmsNotifier.getId() > 0) {
+          this.lastViewedState = psalmsNotifier.id;
+        }
 
-    for (int i = 1; i <= psalmsAmount; i++) {
+        return ListViewWrapper(data: this.renderPsalms(context));
+      },
+    );
+  }
+
+  List<Widget> renderPsalms(BuildContext context) {
+    List<Widget> psalms = [];
+    for (int i = 1; i <= this.psalmsAmount; i++) {
       psalms.add(
-        BookmarkCard(
-          id: i,
-          title: '${'psalm'.tr()} $i',
-          description: '',
-          type: EntityType.psalm,
-          isBookmarked: bookmarks.contains(i),
-        )
+          BookmarkCard(
+            id: i,
+            title: '${context.tr('psalm')} $i',
+            type: EntityType.psalm,
+            isBookmarked: this.bookmarks.contains(i),
+            isActive: (i == this.lastViewedState),
+            notifier: this.psalmsNotifier,
+          )
       );
     }
-    
-    return ListViewWrapper(data: psalms);
+
+    return psalms;
   }
 }
