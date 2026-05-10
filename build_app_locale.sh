046433a7d9c81c27564898c3f5d662d9f98c6408
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-05-10 16:39:17 +0300

    implemented "Common" translation logic for all languages

--- build_app_locale.sh
+++ build_app_locale.sh
@@ -2,13 +2,19 @@
 
 # Translation generator
 echo "Start..."
-dart run arb_merge --sources translations/langs/ru --destination assets/generated/translations --pattern '{lang}.json'
-echo ""
-dart run arb_merge --sources translations/langs/en --destination assets/generated/translations --pattern '{lang}.json'
-echo ""
-dart run arb_merge --sources translations/langs/cu --destination assets/generated/translations --pattern '{lang}.json'
-echo ""
-dart run arb_merge --sources translations/langs/uk --destination assets/generated/translations --pattern '{lang}.json'
-echo ""
+LANGS=(ru en uk)
+
+for lang in ${LANGS[*]}
+do
+    echo "Working on '$lang'..."
+    dart run arb_merge --sources translations/langs/$lang --destination assets/generated/translations --pattern '{lang}.json'
+    # merge with common translation
+    TMPFILE=assets/generated/translations/$lang'_tmp'.json
+    DESTFILE=assets/generated/translations/$lang.json
+    jq -s '.[0] * .[1]' assets/generated/translations/$lang.json translations/langs/common/*.json > $TMPFILE
+    # replace files after merge
+    mv -ifv $TMPFILE $DESTFILE
+    echo ""
+done
 
 echo "Done!"
