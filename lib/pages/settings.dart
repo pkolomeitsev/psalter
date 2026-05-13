a8d4c02e18f6470ebabc59700e385bdb19ba1f00
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-05-13 12:36:40 +0300

    enable English (KJV) translation

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -34,7 +34,7 @@ class _SettingsState extends State<Settings> {
     this.languages = LocaleStorage.languageCodes;
     this.translations = PsalterTranslationStorage.translationCodes;
     // TODO: temporary solution
-    this.tmpDisabled = ['uk', 'kjv'];
+    this.tmpDisabled = ['uk'];
   }
 
   @override
