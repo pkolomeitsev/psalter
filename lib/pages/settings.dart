a8960ef6c8642b5cd82f515a8390cd9c5ee9b924
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-05-28 23:12:02 +0300

    enable UK translation

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -34,7 +34,7 @@ class _SettingsState extends State<Settings> {
     this.languages = LocaleStorage.languageCodes;
     this.translations = PsalterTranslationStorage.translationCodes;
     // TODO: temporary solution
-    this.tmpDisabled = ['uk'];
+    this.tmpDisabled = [];
   }
 
   @override
