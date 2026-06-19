5b0d23911dd61aa88f3cd2d495d85682dcde572e
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-06-19 21:41:31 +0300

    integragte ScrollPositionStorageMixin functionality with Psalms page

--- lib/pages/home/psalms.dart
+++ lib/pages/home/psalms.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:orth_psalter/helpers/debouncer_helper.dart';
+import 'package:orth_psalter/mixins/scroll_position_storage_mixin.dart';
 import 'package:orth_psalter/models/enums/entity_type.dart';
 import 'package:orth_psalter/models/notifiers/last_viewed_psalms_notifier.dart';
 import 'package:orth_psalter/storage/bookmark_storage.dart';
 import 'package:orth_psalter/storage/last_viewed_storage.dart';
 import 'package:orth_psalter/storage/psalm_storage.dart';
-import 'package:orth_psalter/storage/scroll_position_storage.dart';
 import 'package:orth_psalter/ui/components/bookmark_card.dart';
 import 'package:orth_psalter/ui/views/list_view_wrapper.dart';
 
@@ -17,51 +16,22 @@ class Psalms extends StatefulWidget {
   State<Psalms> createState() => _PsalmsState();
 }
 
-class _PsalmsState extends State<Psalms> {
+class _PsalmsState extends State<Psalms> with ScrollPositionStorageMixin {
   int psalmsAmount = PsalmStorage.psalmsAmount;
   int lastViewedId = 0;
   List<int> bookmarks = [];
-  late ScrollController scrollController;
-  final debouncer = DebouncerHelper(milliseconds: 1000);
   final LastViewedPsalmsNotifier lastViewedNotifier =
       LastViewedPsalmsNotifier();
 
   @override
   void initState() {
     super.initState();
-    this.scrollController = ScrollController();
-    this.scrollController.addListener(this.saveScrollPosition);
-  }
-
-  void saveScrollPosition() async {
-    print('call saveScrollPosition()');
-    // Only saves if the position has changed significantly to optimize disk writes
-    debouncer.run(() async {
-      await ScrollPositionStorage.setOffset(
-        EntityType.psalm,
-        this.scrollController.position.pixels,
-        listView: true,
-      );
-    });
+    this.initScrollPositionStorageMixin();
   }
 
   Future fetchActivityData() async {
     this.bookmarks = await BookmarkStorage.getBookmarks(EntityType.psalm);
     this.lastViewedId = await LastViewedStorage().get(EntityType.psalm);
-
-    final scrollValue = await ScrollPositionStorage.getOffset(
-      EntityType.psalm,
-      listView: true,
-    );
-    final double? offset = double.tryParse(scrollValue.toString());
-    if (offset != null && offset > 0) {
-      // Wait for layout rendering to complete before scrolling
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (this.scrollController.hasClients) {
-          this.scrollController.jumpTo(offset);
-        }
-      });
-    }
 
     return true;
   }
@@ -85,7 +55,7 @@ class _PsalmsState extends State<Psalms> {
 
               return ListViewWrapper(
                 data: this.renderPsalms(context),
-                scrollController: this.scrollController,
+                scrollController: this.getScrollController(),
               );
             },
           );
