ed15db9481a64a194b8097c789933519a85fe1c1
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-07-15 00:53:16 +0300

    SimpleDto implementation

---
+++ lib/models/dto/simple_dto.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SimpleDto {
+  String _label;
+  dynamic _value;
+  Icons? _icon = null;
+
+  SimpleDto(this._label, this._value, this._icon);
+
+  String getLabel() => _label;
+  dynamic getValue() => _value;
+  Icons? getIcon() => _icon;
+
+  setIcon(Icons value) {
+    _icon = value;
+  }
+
+  setValue(dynamic value) {
+    _value = value;
+  }
+
+  setLabel(String value) {
+    _label = value;
+  }
+}
