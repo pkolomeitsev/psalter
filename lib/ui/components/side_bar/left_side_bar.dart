9d919d4c43dd861e0f2bc5fea0e3e2e9df13ff33
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-08-23 01:05:25 +0300

    LeftSideBar widget implementation

---
+++ lib/ui/components/side_bar/left_side_bar.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:orth_psalter/theme/app_colors.dart';
+
+class LeftSideBar extends StatefulWidget {
+  const LeftSideBar({super.key});
+
+  @override
+  State<LeftSideBar> createState() => _LeftSideBarState();
+}
+
+class _LeftSideBarState extends State<LeftSideBar> {
+  @override
+  Widget build(BuildContext context) {
+    TextStyle drawerHeaderTextStyle = TextStyle(
+      fontSize: 14,
+      color: AppColors.brandColorLight,
+    );
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            padding: EdgeInsetsGeometry.all(10),
+            decoration: BoxDecoration(color: AppColors.brandBgColor),
+            child: Column(
+              children: [
+                Row(
+                  spacing: 10,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: const CircleAvatar(
+                              backgroundImage: AssetImage(
+                                'assets/imgs/Saint_Gregory_the_Theologian.jpg',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            context.tr('leftSideBarTitle'),
+                            style: drawerHeaderTextStyle
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      context.tr('leftSideBarSubTitle'),
+                      style: drawerHeaderTextStyle,
+                      textAlign: TextAlign.right,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text(context.tr('prayersBeforePsalterReading')),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.add),
+            title: Text(context.tr('prayersAfterPsalterReading')),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
