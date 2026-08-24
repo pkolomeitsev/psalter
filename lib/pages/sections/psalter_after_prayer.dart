8ea38a3db32c00b48a18816cb70d64aa65a9252b
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-08-24 23:05:32 +0300

    init PsalterBeforePrayer, PsalterAfterPrayer pages

---
+++ lib/pages/sections/psalter_after_prayer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PsalterAfterPrayer extends StatefulWidget {
+  const PsalterAfterPrayer({super.key});
+
+  @override
+  State<PsalterAfterPrayer> createState() => _PsalterAfterPrayerState();
+}
+
+class _PsalterAfterPrayerState extends State<PsalterAfterPrayer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/sections/psalter_before_prayer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PsalterBeforePrayer extends StatefulWidget {
+  const PsalterBeforePrayer({super.key});
+
+  @override
+  State<PsalterBeforePrayer> createState() => _PsalterBeforePrayerState();
+}
+
+class _PsalterBeforePrayerState extends State<PsalterBeforePrayer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
