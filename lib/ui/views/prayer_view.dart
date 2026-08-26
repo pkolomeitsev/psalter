956d0a33535ff683ab6e04a6632bb2b1c7a49801
pkolomeitsev <p.kolomeitsev@gmail.com>
2026-08-27 02:33:18 +0300

    PrayerView implementation

---
+++ lib/ui/views/prayer_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:orth_psalter/singleton/appearance_config_singleton.dart';
+import 'package:orth_psalter/theme/app_colors.dart';
+
+class PrayerView extends StatelessWidget {
+  final String prayerName;
+  final String prayerText;
+  const PrayerView({super.key, this.prayerName = '', required this.prayerText});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: [
+        if (this.prayerName.isNotEmpty) ...[
+          SizedBox(height: 10),
+          DefaultTextStyle.merge(
+            style: TextStyle(
+              fontSize: AppearanceConfigSingleton().getTitleFontSize(),
+              fontWeight: FontWeight.bold,
+              color: AppColors.textHeadingColor,
+            ),
+            child: SelectableText(
+              this.prayerName,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+        SizedBox(height: 10),
+        DefaultTextStyle.merge(
+          style: TextStyle(
+            fontSize: AppearanceConfigSingleton().getBodyFontSize(),
+          ),
+          child: SelectableText(this.prayerText),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+}
