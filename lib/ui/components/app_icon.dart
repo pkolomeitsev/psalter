e079457824f5645f8c24183e4ad6b43f5a39eb9c
Pavel Kolomeitsev <p.kolomeitsev@gmail.com>
2026-08-30 23:40:11 +0300

    replaced Cros icon png to svg and vector, update AppIcon widget (#15)

--- lib/ui/components/app_icon.dart
+++ lib/ui/components/app_icon.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+import 'package:orth_psalter/theme/app_colors.dart';
 
 class AppIcon extends StatelessWidget {
-  const AppIcon({super.key});
+  final Color? color;
+  final double? width;
+  final double? height;
+  const AppIcon({super.key, this.width = 25, this.height = 25, this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/icons/orthodox-cross-blue-50.png',
-      height: 50,
+    return SvgPicture(
+      AssetBytesLoader('assets/vector/icons/cros.svg.vec'),
+      height: this.width,
+      width: this.height,
+      colorFilter: ColorFilter.mode(
+        this.color ?? AppColors.brandTextColor,
+        BlendMode.srcIn,
+      ),
     );
   }
 }
--- lib/ui/components/app_title.dart
+++ lib/ui/components/app_title.dart
@@ -1,5 +1,6 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:orth_psalter/ui/components/app_icon.dart';
 
 class AppTitle extends StatelessWidget {
   const AppTitle({super.key});
@@ -9,7 +10,7 @@ class AppTitle extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset('assets/icons/orthodox-cross-white-50.png', height: 24),
+        AppIcon(color: Theme.of(context).appBarTheme.foregroundColor),
         const SizedBox(width: 10),
         Text(context.tr('appTitle')),
       ],
